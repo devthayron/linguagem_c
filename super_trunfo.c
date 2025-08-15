a2fc1db3866ff187fd00265b00964d182c3324af
thayron <devthayronhiglander@gmail.com>
2025-08-15 14:56:30 -0300

    adição do cartão 2

--- super_trunfo.c
+++ super_trunfo.c
@@ -29,13 +29,13 @@ int main(){
 
     printf("Digite a quantidade da área: ");
     scanf("%f",&cartao1.area);
-
+    
     printf("Digite o PIB: ");
     scanf("%f",&cartao1.pib);
-
+    
     printf("Digite a quantidade de pontos turísticos: ");
     scanf("%d",&cartao1.qtd_ponto_turistico);
-
+    printf("-----------------------------------------------------------------------------\n");
 
     printf("Cartão 1\n");
     printf("Estado: %c\n",cartao1.estado);
@@ -45,6 +45,41 @@ int main(){
     printf("área: %.2f\n",cartao1.area);
     printf("PIB: %.2f\n",cartao1.pib);
     printf("Quantidade de pontos turísticos: %d\n",cartao1.qtd_ponto_turistico);
+    printf("-----------------------------------------------------------------------------\n");
+
+    struct cartao cartao2;
+
+    printf("Digite o estado: ");
+    scanf(" %c",&cartao2.estado);
+
+    printf("Digite o codigo: ");
+    scanf("%s",cartao2.codigo);
+
+    printf("Digite o nome da cidade: ");
+    scanf("%s",cartao2.nome_cidade);
+
+    printf("Digite a quantidade de população: ");
+    scanf("%d",&cartao2.populacao);
+
+    printf("Digite a quantidade da área: ");
+    scanf("%f",&cartao2.area);
+
+    printf("Digite o PIB: ");
+    scanf("%f",&cartao2.pib);
+
+    printf("Digite a quantidade de pontos turísticos: ");
+    scanf("%d",&cartao2.qtd_ponto_turistico);
+
+    printf("-----------------------------------------------------------------------------\n");
+
+    printf("Cartão 2\n");
+    printf("Estado: %c\n",cartao2.estado);
+    printf("codigo: %s\n",cartao2.codigo);
+    printf("Nome da Cidade: %s\n",cartao2.nome_cidade);
+    printf("População: %d\n",cartao2.populacao);
+    printf("área: %.2f\n",cartao2.area);
+    printf("PIB: %.2f\n",cartao2.pib);
+    printf("Quantidade de pontos turísticos: %d\n",cartao2.qtd_ponto_turistico);
 
     return 0;
 }
